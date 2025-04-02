2e4e259aed700f311593df1756cf6147a5a70bff
tomas.vince <tomas.vince@asseco-ce.com>
2025-04-02 14:57:57 +0200

    INT: Fixed types in map component

--- packages/components/src/map/Map.web.tsx
+++ packages/components/src/map/Map.web.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 
-export function Map() {
+export function Map({ onMarkerPress }: { onMarkerPress: () => void }) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>🧭 Map is not supported on Web yet.</Text>
