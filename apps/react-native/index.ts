89947068d36ddd07fc13b1a8466c90cfb755cec5
tomas.vince <tomas.vince@asseco-ce.com>
2025-05-16 09:52:47 +0200

    INT: Cleaned tsconfig for react-native app, removed console.log from app initialization

--- apps/react-native/index.ts
+++ apps/react-native/index.ts
@@ -1,7 +1,5 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
-// import {name as appName} from './app.json';
+import {name as appName} from './app.json';
 
-console.log('AppRegistry', AppRegistry);
-// console.log('appName', appName);
-AppRegistry.registerComponent('ReactNativePOC', () => App);
+AppRegistry.registerComponent(appName, () => App);
